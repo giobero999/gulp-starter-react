16fb413f4930e6c117da076841c208d5b99fcf50
giobero999 <giobero999@gmail.com>
2015-07-21 17:19:18 +0400

    first react

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+  react = require('gulp-react'),
+	browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function() {
+    browserSync.init(["app/"],{
+        server: {
+            baseDir: "app/"
+        }
+    });
+});
+
+gulp.task('gulp-react', function () {
+    return gulp.src('app/bundle.jsx')
+        .pipe(react())
+        .pipe(gulp.dest('app/'));
+});
+
+
+//sass
+gulp.task('gulp-sass', function () {
+  gulp.src('app/assets/sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/assets/css'));
+});
+
+
+gulp.task('watch', function(){
+  //gulp.watch('app/index.jade', ['gulp-jade']);
+	gulp.watch('app/assets/sass/*.sass', ['gulp-sass']);
+});
+
+
+
+gulp.task('default', ['browser-sync',  'watch', 'gulp-sass', 'gulp-react']);
